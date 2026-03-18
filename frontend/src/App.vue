3b8c8f63a80f50418b3978afe82145733593f810
ronron <ronflettemaxime@gmail.com>
2026-03-18 12:33:28 +0100

    feat: employee list

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,33 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import EmployeeList from './components/EmployeeList.vue';
+import EmployeeEdit from './components/EmployeeEdit.vue';
+import { ref, computed } from 'vue'
+
+const routes = {
+  '/': EmployeeList,
+  '/detail': EmployeeEdit
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  let uri = currentPath.value.split("/");
+  console.log(uri)
+  if (uri.length == 1){
+    return routes["/"]
+  }
+  else {
+    return routes["/" + uri[1]]
+  }
+  
+})
 </script>
 
 <template>
-  <HelloWorld />
+  <component :is="currentView" />
 </template>
