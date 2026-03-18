52bca349e2349e99b13951d3a16e3a328fcb7acf
ronron <ronflettemaxime@gmail.com>
2026-03-18 15:49:51 +0100

    feat(wip): edit employee

--- frontend/src/components/EmployeeEdit.vue
+++ frontend/src/components/EmployeeEdit.vue
@@ -2,38 +2,89 @@
 import { ref, computed } from 'vue'
 const currentPath = ref(window.location.hash)
 let employeeId =  currentPath.value.split("/")[2]
-const name = ref('')
-const firstName = ref('')
-const phone = ref('')
-const email = ref('')
-const service = ref('')
-const site = ref('')
+const employee = {
+        "id": 1,
+        "name": "Jean",
+        "firstName": "Jean",
+        "phone": "0555555559",
+        "email": "jean.jean@boite.fr",
+        "service": {
+            "id": 2,
+            "name": "Production"
+        },
+        "site": {
+            "id": 2,
+            "name": "Nantes"
+        }
+    }
+
+const services = [
+    {
+        "id": 1,
+        "name": "Comptabilité"
+    },
+    {
+        "id": 2,
+        "name": "Production"
+    },
+    {
+        "id": 3,
+        "name": "Accueil"
+    }
+]
+const name = ref(employee.name)
+const firstName = ref(employee.firstName)
+const phone = ref(employee.phone)
+const email = ref(employee.email)
+const service = ref(employee.service.id)
+const site = ref(employee.site.id)
+
+function send() {
+    employee.name = name.value
+    employee.firstName = firstName.value
+    employee.phone = phone.value
+    employee.email = email.value
+    employee.service = service.value
+    employee.site = site.value
+    console.log(employee)
+}
 </script>
+
 <template>
-    <form>
+    <a href="#/">retour</a>
+    <form @submit.prevent="submit">
         <label>
             Nom
             <input v-model="name">
         </label>
+        <br>
         <label>
             Prénom
             <input v-model="firstName">
         </label>
+        <br>
         <label>
             Téléphone
             <input v-model="phone">
         </label>
+        <br>
         <label>
             Email
             <input v-model="email">
         </label>
+        <br>
         <label>
             Service
-            <input v-model="service">
+            <select v-model="service" >
+                <option v-for="s in services" :value="s.id">{{ s.name }}</option>
+            </select>
         </label>
+        <br>
         <label>
             Site
             <input v-model="site">
         </label>
+        <br>
+        <button @click="send()"> Valider</button>
     </form>
 </template>
